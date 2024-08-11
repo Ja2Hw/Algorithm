bedd385890474d4d60125ed55eb44191eb37efb7
Ja2Hw <86146209+Ja2Hw@users.noreply.github.com>
2024-08-11 10:03:05 +0900

    [Silver V] Title: 단어 정렬, Time: 4448 ms, Memory: 35704 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,12 @@
+n = int(input())
+input_list = []
+tmp = []
+for _ in range(n):
+    word = input()
+    if word not in tmp:
+        tmp.append(word)
+        input_list.append([word, len(word)])
+
+sorted_list = sorted(sorted(input_list), key=lambda x: x[1])
+for s in sorted_list:
+    print(s[0])
