3738f8c4fa727af5d2cd187b9e4cfe1fbf0b98dd
Ja2Hw <86146209+Ja2Hw@users.noreply.github.com>
2024-08-12 23:17:40 +0900

    [Silver V] Title: 영화감독 숌, Time: 612 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+i = 666
+cnt = 0
+
+while True:
+    if '666' in str(i):
+        cnt += 1
+        
+    if cnt == n:
+        break
+        
+    i += 1
+    
+print(i)
+        
+        
