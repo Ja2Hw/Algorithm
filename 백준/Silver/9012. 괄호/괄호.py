89efb59520f3ff62481129bb716ea07f17d0fbd1
Ja2Hw <86146209+Ja2Hw@users.noreply.github.com>
2024-08-19 22:13:01 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,20 @@
+import sys
+
+T = int(sys.stdin.readline())
+PS = [str(input()) for _ in range(T)]
+
+for p in PS:
+    op = 0 # '(' 개수
+    for i in p:
+        if i == '(':
+            op += 1
+        else:
+            op -= 1
+        if op < 0:
+                print("NO")
+                break
+    if op == 0:
+        print("YES")
+    elif op > 0:
+        print("NO")
+        
