2f7146107dd3e48820f711c1f4b48093210d53d7
Ja2Hw <86146209+Ja2Hw@users.noreply.github.com>
2024-08-20 11:13:09 +0900

    [Silver II] Title: 스택 수열, Time: 2512 ms, Memory: 32704 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,28 @@
+import sys
+
+N = int(input())
+
+stk = [] # 메인 스택
+result = [] # 결과 +- 들어감
+stop = False
+
+tmp = 1 #시작하는 수
+
+for _ in range(N):
+    num = int(input())
+    while tmp <= num: # 찾고 있던 숫자 나오기 전까지 push (+)
+        stk.append(tmp)
+        result.append('+')
+        tmp += 1
+    if stk[-1] == num: # 메인 스택 마지막(마지막에 넣은 tmp)이 num일 때 = 찾고 있던 숫자일 때
+        stk.pop()
+        result.append('-')
+    else:
+        stop = True
+        
+if stop:
+    print("NO")
+else:
+    for r in result:
+        print(r)
+    
