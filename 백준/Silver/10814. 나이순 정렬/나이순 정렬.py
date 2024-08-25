88db94a1dec1abde3d3eb59ff5a77e3eb6c86055
Ja2Hw <86146209+Ja2Hw@users.noreply.github.com>
2024-08-25 14:09:18 +0900

    [Silver V] Title: 나이순 정렬, Time: 248 ms, Memory: 48188 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.readline())
+members = []
+
+for _ in range(N):
+    age, name = map(str, sys.stdin.readline().split())
+    members.append([int(age), name])
+                    
+members.sort(key = lambda x : x[0])	## (age, name)에서 age만 비교
+
+for m in members:
+    print(*m)
+
+
+#print(*res) # print(*list)는 리스트 원소들 사이에 공백 주고 모두 출력하는 것
