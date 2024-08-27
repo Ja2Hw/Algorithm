a0dc93080349e02b19ce4ec8d42be7a2d5b5a672
Ja2Hw <86146209+Ja2Hw@users.noreply.github.com>
2024-08-27 20:25:57 +0900

    [Silver IV] Title: 제로, Time: 60 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+import sys
+
+K = int(sys.stdin.readline())
+res = []
+for _ in range(K):
+    n = int(sys.stdin.readline())
+    if n == 0:
+        res.pop()
+    else:
+        res.append(n)
+        
+print(sum(res))
