5c81117f91d1247a134c7fea33830bfc31af3e07
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2024-10-21 23:53:57 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+import sys
+
+n = sys.stdin.readline().strip()
+
+if n == 'SONGDO':
+    print('HIGHSCHOOL')
+elif n == 'CODE':
+    print('MASTER')
+elif n == '2023':
+    print('0611')
+else:
+    print('CONTEST')
