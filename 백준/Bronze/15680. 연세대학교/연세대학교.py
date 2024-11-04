bfb56d95c98337c063f00707baef219e53ef05ab
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2024-11-04 23:40:41 +0900

    [Bronze V] Title: 연세대학교, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.py
@@ -0,0 +1,9 @@
+#연세대학교의 영문명은 YONSEI, 슬로건은 Leading the Way to the Future
+import sys
+
+n = int(sys.stdin.readline())
+
+if n == 0:
+    print("YONSEI")
+else:
+    print("Leading the Way to the Future")
