e10337db371f1506a7f8c100c1863f78099f1ce0
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2024-11-19 23:52:51 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,12 @@
+import sys
+
+n = sys.stdin.readline().strip()
+
+if n=='NLCS':
+    print('North London Collegiate School')
+elif n=='BHA':
+    print('Branksome Hall Asia')
+elif n=='KIS':
+    print('Korea International School')
+else:
+    print('St. Johnsbury Academy')
