656f0e6b22a63ab2213f4a6941510f2e60365f06
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2025-01-06 15:10:05 +0900

    [Bronze I] Title: 단어 공부, Time: 72 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -3,6 +3,25 @@ import sys
 word = sys.stdin.readline().strip().upper()
 
 set_word = set(word)
+
+# 등장 횟수를 저장할 딕셔너리
+count_dict = {char: word.count(char) for char in set_word}
+
+# 최댓값 찾기
+max_count = max(count_dict.values())
+
+# 최댓값에 해당하는 문자 찾기
+max_chars = [char for char, count in count_dict.items() if count == max_count]
+
+# 출력
+if len(max_chars) == 1:
+    print(max_chars[0])
+else:
+    print("?")
+
+"""
+처음 풀어서 맞춘 방식
+
 cnt = 0
 only = True
 for w in set_word:
@@ -18,3 +37,4 @@ if only:
     print(res)
 else:
     print("?")
+"""
