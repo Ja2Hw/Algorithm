a60457481e02bdb20cec47c92a6f1147721086fa
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2025-01-15 20:02:31 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,14 @@
+import sys
+
+n = sys.stdin.readline().strip()
+
+if n == 'M':
+    print("MatKor")
+elif n == 'W':
+    print("WiCys")
+elif n == 'C':
+    print("CyKor")
+elif n == 'A':
+    print("AlKor")
+else:
+    print("$clear")
