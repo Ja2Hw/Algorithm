40d93d03031cd81fa473b684ab5e5a1dd9d7b804
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2025-01-20 20:28:24 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
@@ -0,0 +1,21 @@
+# 2755 이번 학기 평점은 몇점?
+# 브론즈 1
+
+import sys
+
+n = int(sys.stdin.readline())
+
+d = {"A+": 4.3, "A0": 4.0, "A-": 3.7, "B+": 3.3, "B0": 3.0, 
+     "B-": 2.7, "C+": 2.3, "C0": 2.0, "C-": 1.7, "D+": 1.3, 
+     "D0": 1.0, "D-": 0.7, "F": 0.0}
+
+cnt = 0
+res = 0
+
+for _ in range(n) :
+  sub, lv, score = sys.stdin.readline().split()
+  lv = int(lv)
+  res += lv * d[score]
+  cnt += lv
+
+print("%.2f" % (round(res/cnt + 10**-10, 2)))
