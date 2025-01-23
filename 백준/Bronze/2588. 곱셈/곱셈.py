690d21ed963d0da5cedcdf4a5440066c073cda6e
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2025-01-23 20:16:01 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,13 @@
+# 2588 곱셈
+# 브론즈 3
+
+import sys
+
+a = int(sys.stdin.readline().strip())
+b = sys.stdin.readline().strip()
+
+print(a * int(b[2]))
+print(a * int(b[1]))
+print(a * int(b[0]))
+
+print(a * int(b))
