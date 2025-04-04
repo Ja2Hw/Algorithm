db633c2e13b3f2d5007db156526e61b0574714b2
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2025-04-04 20:18:31 +0900

    [Bronze IV] Title: 빵, Time: 40 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -8,12 +8,13 @@ n = int(sys.stdin.readline())
 res = -1
 
 for _ in range(n):
+    #현재 위치에서 가게까지 가는 데 걸리는 시간 A
+    #현재 시점에서 빵이 들어올 때까지 시간 B
     a, b = map(int, sys.stdin.readline().split())
     if (a-b) <= 0: # 구매 성공
-        if res == 0:
+        if res == -1:
             res = b
         elif b < res:
             res = b
 
 print(res)
-        
