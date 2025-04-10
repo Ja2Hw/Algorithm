d05c89f36e6989ae18398cabd6cebff28228b988
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2025-04-10 22:33:46 +0900

    [Bronze III] Title: 369, Time: 532 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -5,7 +5,10 @@ n = int(sys.stdin.readline())
 cnt = 0
 
 for i in range(1, n+1):
-    if '3' in str(i) or '6' in str(i) or '9' in str(i):
-        cnt += 1
-
+    s = str(i)
+    
+    cnt += s.count('3')
+    cnt += s.count('6')
+    cnt += s.count('9')
+    
 print(cnt)
