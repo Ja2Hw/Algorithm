1f909c7c6fbb507400b97b7dd194efe20614f627
JaeHwan Kim <86146209+Ja2Hw@users.noreply.github.com>
2025-05-24 15:17:46 +0900

    Update 등차수열의 합.py

--- 백준/Bronze/28115. 등차수열의 합/등차수열의 합.py
+++ 백준/Bronze/28115. 등차수열의 합/등차수열의 합.py
@@ -15,8 +15,8 @@ if n == 1:
     print(*A) # 그대로 등차수열
 else:
     d = A[1] - A[0]
-    if any(A[i] - A[i-1] != d for i in range(2, n)):
-        print("NO")
+    if any(A[i] - A[i-1] != d for i in range(2, n)):  # any(): 모든 값이 같으면 False, 다른 게 있으면 True 리턴
+        print("NO") # 다른 게 발견되어 any()가 True를 리턴했으므로 등차수열이 아님
     else:
         print("YES")
         zero = [0]*n    
